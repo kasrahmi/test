1cb4ff6808783d77c415816641c00b70187cfc52
kasrahmi <119696431+kasrahmi@users.noreply.github.com>
2022-12-03 09:38:59 +0330

    Update code.c

--- code.c
+++ code.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+	//amirkasra ahmadi 401170507
 
 int main() {
 	//test 
 	int n;
 	scanf("%d" , &n);
 	int x = n * n;
+	printf("square n : ");
 	printf("%d" , x);
 	return 0;
 }
